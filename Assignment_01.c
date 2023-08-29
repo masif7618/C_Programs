a39cc080b6990a45bc70d093bcef37331dae60cb
Asif <moasifkhan8@gmail.com>
2023-08-29 13:40:26 -0700

    For Test

--- Assignment_01.c
+++ Assignment_01.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+// Assignment-1 : Input and output in C Language
 
+/* 1). Write a to print Hello Students on the screen.
 int main(){
-    printf("Hello Ali");
+    printf("Hello Students");
     return 0;
 }
+*/
 
 
 
